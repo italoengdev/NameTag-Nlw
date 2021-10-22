fce3a9ec436a6270f6d6818ea683fe2fdeca9e6d
Italo Saraiva.Dev <92400207+italoengdev@users.noreply.github.com>
2021-10-22 10:41:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const linksSocialMedia = {
+  github: 'italoengdev',
+  youtube: 'italosaraiva',
+  facebook: 'italosaraiva',
+  instagram: 'italosaraiva',
+  twitter: 'italo_sg'
+}
+//DOM document object model
+
+//camelCase
+//PascalCase
+//snake_case
+
+function changeSocialMediaLinks() {
+  // mesma coisa que userName.textContente -->  document.getElementById('userName').textContent = 'Olivia'
+
+  // estrutura for --> for(let i = 0;i <= 10; i++){
+  //alert(i)
+  //}
+  //socialLinks.textContent = socialLinks.children
+
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
